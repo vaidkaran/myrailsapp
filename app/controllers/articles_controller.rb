9b7308ba663cef4deca771b059baacf520d92743
karan <karan@ubuntu.(none)>
2014-09-06 15:32:11 +0530

    implemented edit and show links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,16 @@ class ArticlesController < ApplicationController
    @article = Article.find(params[:id]) 
   end
 
+  def update
+    @article = Article.find(params[:id]) 
+
+    if @article.update(article_params)
+      redirect_to @article 
+    else
+      render 'edit'
+    end
+  end
+
  	def index
  		@articles = Article.all
  	end
